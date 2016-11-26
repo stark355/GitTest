65efd79f29c1198ff0c70cbcce520ea04c359877
stark355 <rudakov-dbn@yandex.ru>
2016-11-26 02:28:40 -0800

    Second row

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Smth first");
+            Console.WriteLine("Smth second");
         }
     }
 }
